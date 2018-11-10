7a0222c1ef06106d115527fc3a5e48f6da8b07fb
Lino Valdivia <lino@valdivia.com>
2018-11-10 14:48:25 +0100

    Using sl4j for logging (thanks Spring)

--- src/main/java/org/lagalag/web/controller/PlaceSenseController.java
+++ src/main/java/org/lagalag/web/controller/PlaceSenseController.java
@@ -7,6 +7,8 @@ import org.lagalag.web.model.transfer.SavePlaceSenseResponseDTO;
 import org.lagalag.web.service.PlaceSenseService;
 import org.lagalag.web.service.PlaceSenseStats;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PlaceSenseController {
+    private static final Logger logger = LoggerFactory.getLogger("lagalag");
+    
     @Autowired
     private ModelMapper dtoEntityMapper;
     
@@ -22,15 +26,16 @@ public class PlaceSenseController {
     
     @PostMapping("/place-sense")
     public SavePlaceSenseResponseDTO savePlaceSense(@RequestBody PlaceSenseDTO placeSenseDTO) {
-        System.out.println("*** RECEIVED: " + placeSenseDTO);
+        logger.debug("Received: " + placeSenseDTO);
         PlaceSense placeSense = dtoEntityMapper.map(placeSenseDTO, PlaceSense.class);
-        System.out.println("Entity: " + placeSense);
-        placeSense = placeSenseService.save(placeSense);
+        logger.debug("Mapped to Entity: " + placeSense);
+        PlaceSense savedPlaceSense = placeSenseService.save(placeSense);
         
         SavePlaceSenseResponseDTO respDTO = new SavePlaceSenseResponseDTO();
-        respDTO.placeSenseId = placeSense.getId();
+        respDTO.placeSenseId = savedPlaceSense.getId();
         PlaceSenseStats stats = placeSenseService.getStats();
         respDTO.stats = dtoEntityMapper.map(stats, PlaceSenseStatsDTO.class);
+        logger.debug("Returning: " + respDTO);
         return respDTO;
     }
 }
